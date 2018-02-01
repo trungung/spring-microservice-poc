2fd6d43707aed9e6c9de2cf584e8ad1ea4b70272
Trung Ung <trung.ungtoan@asnet.com.vn>
2018-02-01 10:46:32 +0700

    #1 fist init codebase example

---
+++ sample-ping/src/main/java/org/bk/consumer/domain/MessageAcknowledgement.java
@@ -0,0 +1,55 @@
+package org.bk.consumer.domain;
+
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+public class MessageAcknowledgement {
+
+    private String id;
+    private String received;
+    private String payload;
+
+    public MessageAcknowledgement() {
+
+    }
+
+    public MessageAcknowledgement(String id, String received, String payload) {
+        this.id = id;
+        this.received = received;
+        this.payload = payload;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getReceived() {
+        return received;
+    }
+
+    public void setReceived(String received) {
+        this.received = received;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("received", received)
+                .add("payload", payload)
+                .toString();
+    }
+}
